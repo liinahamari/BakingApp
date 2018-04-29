4aaa348436dd2b49458a7fdfdadb5ec38552ba44
l1maginaire <l1maginaire00@gmail.com>
2018-04-29 15:04:47 +0300

    DetailFragment recycler"s mapping

---
+++ app/src/main/java/com/example/guest/bakingapp/adapters/StepsAdapter.java
@@ -0,0 +1,65 @@
+package com.example.guest.bakingapp.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.guest.bakingapp.R;
+import com.example.guest.bakingapp.mvp.model.Step;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by l1maginaire on 4/29/18.
+ */
+
+public class StepsAdapter extends RecyclerView.Adapter<StepsAdapter.ViewHolder> {
+    private List<Step> steps;
+    private Context context;
+
+    public StepsAdapter(List<Step> steps, Context context) {
+        this.steps = steps;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public StepsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.single_step, parent, false);
+        return new StepsAdapter.ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StepsAdapter.ViewHolder holder, int position) {
+        StringBuilder builder = new StringBuilder();
+        holder.label.setText(steps.get(position).getShortDescription());
+        holder.description.setText(steps.get(position).getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return (steps == null) ? 0 : steps.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.step_label)
+        protected TextView label;
+        @BindView(R.id.step_description)
+        protected TextView description;
+        private final View view;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            view = itemView;
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
+
--- app/src/main/java/com/example/guest/bakingapp/ui/DetailFragment.java
+++ app/src/main/java/com/example/guest/bakingapp/ui/DetailFragment.java
@@ -6,12 +6,17 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.guest.bakingapp.R;
+import com.example.guest.bakingapp.adapters.MainListAdapter;
+import com.example.guest.bakingapp.adapters.StepsAdapter;
 import com.example.guest.bakingapp.mvp.model.Reciep;
 import com.example.guest.bakingapp.utils.MakeIngredietsString;
 
@@ -27,9 +32,12 @@ import static com.example.guest.bakingapp.ui.DetailActivity.ID;
 public class DetailFragment extends Fragment {
     @BindView(R.id.ingredients_tv)
     protected TextView ingredientsTv;
+    @BindView(R.id.detail_recycler)
+    protected RecyclerView recyclerView;
 
     private Callbacks callbacks;
     private Reciep reciep;
+    private StepsAdapter adapter;
 
     public static DetailFragment newInstance(Reciep reciep) {
         Bundle args = new Bundle();
@@ -55,7 +63,14 @@ public class DetailFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_details, container, false);
         ButterKnife.bind(this, v);
         setView();
+        setupAdapter();
         return v;
+    }
+
+    private void setupAdapter() {
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        adapter = new StepsAdapter(reciep.getSteps(), getActivity());
+        recyclerView.setAdapter(adapter);
     }
 
     private void setView() {
