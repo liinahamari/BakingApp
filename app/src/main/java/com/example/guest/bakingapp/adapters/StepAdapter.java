0e6640796ed33fab3e221c53ea23d541f08c06d4
l1maginaire <l1maginaire00@gmail.com>
2018-05-07 18:11:07 +0300

    Separated StepAdapter class

--- app/src/main/java/com/example/guest/bakingapp/adapters/StepAdapter.java
+++ app/src/main/java/com/example/guest/bakingapp/adapters/StepAdapter.java
@@ -1,45 +1,45 @@
-/*
 package com.example.guest.bakingapp.adapters;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
-import com.example.guest.bakingapp.mvp.model.Step;
+
+import com.example.guest.bakingapp.data.remote.pojo.StepRemote;
+import com.example.guest.bakingapp.ui.StepFragment;
 
 import java.util.List;
+import java.util.Locale;
 
-*/
 /**
- * Created by l1maginaire on 4/29/18.
- *//*
+Created by l1maginaire on 4/29/18.
+*/
 
+public class StepAdapter extends FragmentPagerAdapter {
+    private List<StepRemote> stepList;
+    private String title;
 
-public class StepAdapter extends FragmentStatePagerAdapter {
-    private List<StepLocal> stepList;
-
-    public StepsAdapter(FragmentManager fm) {
+    public StepAdapter(FragmentManager fm, List<StepRemote> stepList) {
         super(fm);
-    }
-
-    public void setData(List<StepLocal> stepList) {
         this.stepList = stepList;
         notifyDataSetChanged();
     }
 
     @Override
     public Fragment getItem(int position) {
-        Bundle args = new Bundle();
-        args.putString(REVIEW_DATA, stepList.get(position).getContent());
-        args.putString(REVIEW_AUTHOR, stepList.get(position).getAuthor());
-        ReviewFragment fragment = new ReviewFragment();
-        fragment.setArguments(args);
-        return fragment;
+        return StepFragment.newInstance(stepList.get(position).getVideoURL(),
+                stepList.get(position).getDescription());
     }
 
     @Override
     public int getCount() {
         return (stepList == null || stepList.size() < 1) ? 0 : stepList.size();
     }
-}*/
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return String.format(Locale.US, title, position);
+    }
+}
