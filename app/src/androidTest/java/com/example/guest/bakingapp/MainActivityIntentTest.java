79287a1459f42a78493f9cc933a08331cff1186b
l1maginaire <l1maginaire00@gmail.com>
2018-05-18 22:09:10 +0300

    MainActivityIntentTest accomplished

--- app/src/androidTest/java/com/example/guest/bakingapp/MainActivityIntentTest.java
+++ app/src/androidTest/java/com/example/guest/bakingapp/MainActivityIntentTest.java
@@ -1,5 +1,6 @@
 package com.example.guest.bakingapp;
 
+import android.support.test.espresso.IdlingRegistry;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.espresso.matcher.ViewMatchers;
@@ -8,6 +9,8 @@ import android.support.test.runner.AndroidJUnit4;
 
 import com.example.guest.bakingapp.ui.MainActivity;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -31,17 +34,21 @@ public class MainActivityIntentTest {
     public IntentsTestRule<MainActivity> intentsTestRule
             = new IntentsTestRule<>(MainActivity.class);
 
+    @Before
+    public void registerIdlingResource() {
+        IdlingRegistry.getInstance().register(intentsTestRule.getActivity().getIdlingResource());
+    }
+
     @Test
     public void clickOnRecyclerViewItem_runsRecipeDetailsActivityIntent() {
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         onView(ViewMatchers.withId(R.id.main_recycler))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
 
         intended(hasExtra(ID, EXTRA_RECIPE_ID_VALUE));
     }
 
+    @After
+    public void unregisterIdlingResource() {
+        IdlingRegistry.getInstance().unregister(intentsTestRule.getActivity().getIdlingResource());
+    }
 }
